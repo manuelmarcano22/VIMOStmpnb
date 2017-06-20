85ee5d4301171d4d4755f4e59cf64ac0c4a63929
Manuel Marcano <manuelmarcano22@yahoo.com>
2017-06-20 12:29:07 -0500

    Trusted notebooks. Need to tests

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ docker pull jupyter/tmpnb
 docker build -f Dockerfile -t trytemp .
 export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
-docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image='trytemp' --command='source activate iraf27; jupyter notebook --allow-root  --no-browser --NotebookApp.base_url={base_path} --NotebookApp.token=""  --ip=0.0.0.0 --port {port}'
+
+docker run --net=host -it -e CONFIGPROXY_AUTH_TOKEN=$TOKEN -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image='trytemp' --command='
+source activate iraf27; trustn=$(find VIMOSReduced/ -not -path *.ipynb_checkpoints/*   -name *.ipynb); for i in $trustn; do jupyter trust $i; done ;jupyter notebook --allow-root  --no-browser --NotebookApp.base_url={base_path} --NotebookApp.token=""  --ip=0.0.0.0 --port {port}'
+
+
 
