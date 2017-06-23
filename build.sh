1bc60bfd6dfb8d53335bbbb4b3b060f8aaa6a980
Manuel Marcano <manuelmarcano22@yahoo.com>
2017-06-23 16:05:25 -0500

    First draft template

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
 
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image='trytemp' --command='
-source activate iraf27; trustn=$(find VIMOSReduced/ -not -path *.ipynb_checkpoints/*   -name *.ipynb); for i in $trustn; do jupyter trust $i; done ;jupyter notebook --allow-root  --no-browser --NotebookApp.base_url={base_path} --NotebookApp.token=""  --ip=0.0.0.0 --port {port}'
+source activate iraf27; trustn=$(find VIMOSReduced/ -not -path *.ipynb_checkpoints/*   -name *.ipynb); for i in $trustn; do jupyter trust $i; done ;jupyter notebook --allow-root  --no-browser --NotebookApp.base_url={base_path} --NotebookApp.token="" --NotebookApp.iopub_data_rate_limit=100000000  --ip=0.0.0.0 --port {port}'
 
 
 
